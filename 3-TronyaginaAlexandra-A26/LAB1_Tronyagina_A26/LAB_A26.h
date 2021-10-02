b79eb6275786d78e673ed46b3b10bfe4e0429b74
tronyaginaa <tronyaginaa@gmail.com>
2021-10-02 21:48:43 +0300

    Create LAB_A26.h

---
+++ 3-TronyaginaAlexandra-A26/LAB1_Tronyagina_A26/LAB_A26.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+#pragma warning (disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 12
+
+    typedef struct list {
+        char date[N];
+        int temp;
+        struct list* ptr;
+    }list;
+
+    list* listInitialization(char* date, int temp);
+    int dateComparison(char* first, char* second);
+    list* push(list* head, char* date, int temp);
+    list* readFile(char const* fileName);
+    int weatherBelowZero(list* head);
+    int averageTemperatureSearch(list* head, int temp);
+    void deleteList(list* head);
+    list* deleteFirstElement(list* head);
+
+#ifdef __cplusplus
+}
+#endif
