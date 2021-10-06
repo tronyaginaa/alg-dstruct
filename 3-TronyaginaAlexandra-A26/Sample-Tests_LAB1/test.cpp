4d2de1cf57b603d44fd76b963087bbc712861cc2
tronyaginaa <tronyaginaa@gmail.com>
2021-10-07 00:21:44 +0300

    Исправлены и добавлены тесты, указанные в комментариях.

--- 3-TronyaginaAlexandra-A26/Sample-Tests_LAB1/test.cpp
+++ 3-TronyaginaAlexandra-A26/Sample-Tests_LAB1/test.cpp
@@ -9,6 +9,25 @@ TEST(dateComparison, comparisonEarlyWLaterDate) {
 	EXPECT_EQ(1, dateComparison(date_1,date_2));
 }
 
+TEST(dateComparison, comparisonTheSameDates) {
+	char date_1[] = "2020-11-16";
+	char date_2[] = "2020-11-16";
+	EXPECT_EQ(0, dateComparison(date_1, date_2));
+}
+TEST(dateComparison, comparisonDatesWDifferentYears) {
+	char date_1[] = "2002-6-1";
+	char date_2[] = "2020-11-16";
+	EXPECT_EQ(1, dateComparison(date_1, date_2));
+}
+
+TEST(dateComparison, comparisonDatesWSameYears) {
+	char date_1[] = "2020-12-1";
+	char date_2[] = "2020-9-8";
+	EXPECT_EQ(0, dateComparison(date_1, date_2));
+}
+
+
+
 TEST(listInitialization, addFirstElement) {
 	list* head = NULL;
 	head = listInitialization("2010-6-15", 10);
@@ -23,10 +42,10 @@ TEST(push, addNewElement) {
 	head = push(head, "2001-12-3", 20);
 	EXPECT_EQ(head->temp, 15);
 	EXPECT_STREQ(head->date, "2018-9-11");
-	head = head->ptr;
-	EXPECT_EQ(head->temp, 20);
-	EXPECT_STREQ(head->date, "2001-12-3");
-	deleteList(head);
+	EXPECT_EQ(head->ptr->temp, 20);
+	EXPECT_STREQ(head->ptr->date, "2001-12-3");
+	free(head->ptr);
+	free(head);
 }
 
 TEST(push,sortByDTemperature) {
@@ -35,9 +54,16 @@ TEST(push,sortByDTemperature) {
 	head = push(head, "2002-9-2", -10);
 	head = push(head, "2003-9-1", 25);
 	EXPECT_TRUE(head->temp < head->ptr->temp);
-	head = head->ptr;
-	EXPECT_TRUE(head->temp < head->ptr->temp);
-	deleteList(head);
+	EXPECT_EQ(head->temp, -10);
+	EXPECT_STREQ(head->date, "2002-9-2");
+	EXPECT_TRUE(head->ptr->temp < head->ptr->ptr->temp);
+	EXPECT_EQ(head->ptr->temp, 15);
+	EXPECT_STREQ(head->ptr->date, "2018-9-11");
+	EXPECT_EQ(head->ptr->ptr->temp, 25);
+	EXPECT_STREQ(head->ptr->ptr->date, "2003-9-1");
+	free(head->ptr->ptr);
+	free(head->ptr);
+	free(head);
 }
 
 TEST(push, sortByDate) {
@@ -45,10 +71,14 @@ TEST(push, sortByDate) {
 	head = push(head, "2018-10-11", -20);
 	head = push(head, "2003-9-2", -20);
 	head = push(head, "2003-9-1", -20);
+	EXPECT_EQ(head->temp, head->ptr->temp);
+	EXPECT_EQ(head->temp, head->ptr->ptr->temp);
 	EXPECT_STREQ(head->date, "2003-9-1");
 	EXPECT_STREQ(head->ptr->date, "2003-9-2");
 	EXPECT_STREQ(head->ptr->ptr->date, "2018-10-11");
-	deleteList(head);
+	free(head->ptr->ptr);
+	free(head->ptr);
+	free(head);
 }
 
 
@@ -90,15 +120,15 @@ TEST(readFile, normalFileFor3Elements) {
 	ASSERT_FALSE(head == NULL);
 	EXPECT_EQ(head->temp, -10);
 	EXPECT_STREQ(head->date, "2020-12-1");
-	head = head->ptr;
 	ASSERT_FALSE(head == NULL);
-	EXPECT_EQ(head->temp, 0);
-	EXPECT_STREQ(head->date, "2020-3-4");
-	head = head->ptr;
+	EXPECT_EQ(head->ptr->temp, 0);
+	EXPECT_STREQ(head->ptr->date, "2020-3-4");
 	ASSERT_FALSE(head == NULL);
-	EXPECT_EQ(head->temp,25);
-	EXPECT_STREQ(head->date, "2020-6-1");
-	deleteList(head);
+	EXPECT_EQ(head->ptr->ptr->temp,25);
+	EXPECT_STREQ(head->ptr->ptr->date, "2020-6-1");
+	free(head->ptr->ptr);
+	free(head->ptr);
+	free(head);
 }
 
 TEST(averageTemperatureSearch, findTwoElementsWAverageTemperature) {
@@ -111,6 +141,16 @@ TEST(averageTemperatureSearch, findTwoElementsWAverageTemperature) {
 	EXPECT_EQ(averageTemperatureSearch(&list_1, 0), 2);
 }
 
+TEST(averageTemperatureSearch, findThreeElementsWAverageTemperature) {
+	list list_1, list_2, list_3;
+	list_1 = { "2003-1-4", 10 };
+	list_2 = { "2004-5-6", 10 };
+	list_3 = { "2014-3-11", 10 };
+	list_2.ptr = &list_3;
+	list_1.ptr = &list_2;
+	EXPECT_EQ(averageTemperatureSearch(&list_1, 10), 3);
+}
+
 TEST(averageTemperatureSearch, noElementsWAverageTemperature) {
 	list list_1, list_2, list_3;
 	list_1 = { "2003-1-4", -15};
@@ -121,12 +161,9 @@ TEST(averageTemperatureSearch, noElementsWAverageTemperature) {
 	EXPECT_EQ(averageTemperatureSearch(&list_1, 0), 0);
 }
 
-
 int main(void) {
-
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
 	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
-	RUN_ALL_TESTS();
-	return 0;
+	return RUN_ALL_TESTS();
 }
