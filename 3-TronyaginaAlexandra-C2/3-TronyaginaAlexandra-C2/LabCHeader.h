8da5b86f6fb82914b63e5fb5ebd1ac03ab97315c
tronyaginaa <tronyaginaa@gmail.com>
2021-11-21 13:25:39 +0300

    Update LabCHeader.h

--- 3-TronyaginaAlexandra-C2/3-TronyaginaAlexandra-C2/LabCHeader.h
+++ 3-TronyaginaAlexandra-C2/3-TronyaginaAlexandra-C2/LabCHeader.h
@@ -11,24 +11,22 @@ extern "C" {
 	#define TRUE 1
 	#define FALSE (!TRUE)
 
-    typedef struct node_t{
+    typedef struct node_t {
         int value;
         struct node_t* nextNode;
     } node_t;
 
-    typedef struct myStack_t{
+    typedef struct myStack_t {
         node_t* head;
     } myStack_t;
 
 // stack functions
-	int pop(myStack_t** stack);
+	int pop(myStack_t* stack);
 	myStack_t* push(myStack_t* stack, int value);
 
-	void fillGraph();
-	void memoryInit();
-	void memoryFree(myStack* stack, int* checkedVertices, int** graph);
-	void readGraph();
-	void DFS(int num, int** graph, int* chekedVertices, int numOfVertices, myStack_t* stack);
+	void fillGraph(int** graph, int numOfVertices);
+	void readGraph(FILE* stream, int** graph);
+	void DFS(int num, int** graph, int numOfVertices);
 	
 
 #ifdef __cplusplus
