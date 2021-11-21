c72c36319aa6ae472b22602f80167eb44bee5f6d
tronyaginaa <tronyaginaa@gmail.com>
2021-11-21 13:25:18 +0300

    Update test.cpp

--- 3-TronyaginaAlexandra-C2/LabCTests/test.cpp
+++ 3-TronyaginaAlexandra-C2/LabCTests/test.cpp
@@ -15,7 +15,7 @@
 
 #define TEST_NUM_OF_VERTICES 25000
 
-TEST(DFS_test, FillingFileForStressTest){
+TEST(DFS_test, FillingFileForStressTest) {
     srand(time(NULL));
     int** graphToFeelFile = (int**)malloc(TEST_NUM_OF_VERTICES * sizeof(int*));
     ASSERT_TRUE(graphToFeelFile);
@@ -32,10 +32,9 @@ TEST(DFS_test, FillingFileForStressTest){
         for (int j = i + 1; j < TEST_NUM_OF_VERTICES; j++)
             graphToFeelFile[i][j] = graphToFeelFile[j][i] = rand() % 2;
     FILE* f = fopen("graph_test.txt", "w");
-    if (f == NULL) {
+    if (!f) {
         for (int i = 0; i < TEST_NUM_OF_VERTICES; i++)
-            for (int j = 0; j < TEST_NUM_OF_VERTICES; j++)
-                free(graphToFeelFile[j]);
+            free(graphToFeelFile[i]);
         free(graphToFeelFile);
         ASSERT_TRUE(f);
     }
@@ -50,50 +49,32 @@ TEST(DFS_test, FillingFileForStressTest){
     free(graphToFeelFile);
 }
 
-TEST(DFS_test, StressTest){
+TEST(DFS_test, StressTest) {
     int first, second, numOfVertices;
-    FILE*f = fopen("graph_test.txt", "r");
+    FILE* f = fopen("graph_test.txt", "r");
     ASSERT_TRUE(f);
     fscanf(f, "%i", &numOfVertices);
-    int* checkedVertices = (int*)malloc(numOfVertices * sizeof(int));
-    if (!checkedVertices) {
-        fclose(f);
-        ASSERT_TRUE(checkedVertices);
-    }
-    myStack* stack = (myStack*)malloc(myStack);
-    if (!stack) {
-        free(checkedVertices);
-        fclose(f);
-        ASSERT_TRUE(stack);
-    }
-    stack->head = NULL;
     int** graph = (int**)malloc(numOfVertices * sizeof(int*));
-    if(!graph) {
-        free(checkedVertices);
-        free(stack);
+    if (!graph) {
         fclose(f);
         ASSERT_TRUE(graph);
     }
     for (int i = 0; i < numOfVertices; i++) {
         graph[i] = (int*)malloc(numOfVertices * sizeof(int));
-        if (!graph[i]) { 
+        if (!graph[i]) {
             for (int j = 0; j < i; j++)
                 free(graph[j]);
             free(graph);
-            free(checkedVertices);
-            free(stack);
             fclose(f);
             ASSERT_TRUE(!graph);
-            }
-        checkedVertices[i] = FALSE;
+        }
         for (int j = 0; j < numOfVertices; j++)
             graph[i][j] = FALSE;
     }
     while (fscanf(f, "%i %i", &first, &second) != EOF) 
         graph[first][second] = graph[second][first] = TRUE;
-    DFS(0, graph, checkedVertices, numOfVertices, stack);
+    DFS(0, graph, numOfVertices);
     fclose(f);
-    free(checkedVertices);
     for (int i = 0; i < numOfVertices; i++)
         free(graph[i]);
     free(graph);
