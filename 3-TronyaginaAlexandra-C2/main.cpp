82aaa3ba003cb1ef2f4eb2961d4ea120cfc95936
tronyaginaa <tronyaginaa@gmail.com>
2021-11-24 07:12:27 +0300

    Update main.cpp

--- 3-TronyaginaAlexandra-C2/main.cpp
+++ 3-TronyaginaAlexandra-C2/main.cpp
@@ -33,9 +33,9 @@ int pop(myStack_t* stack) {
 
 void stackDestroy(myStack_t* stack) {
 	node_t* node = stack->head;
-    node_t* nodeForFree;
+	node_t* nodeForFree;
 	while (node) {
-        nodeForFree = node;
+		nodeForFree = node;
 		node = node->nextNode;
 		free(nodeForFree);
 	}
