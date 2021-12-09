bf065df78e037d8241198ea09331f0eced5b12cd
tronyaginaa <tronyaginaa@gmail.com>
2021-12-09 23:03:06 +0300

    Create LabDHeader.h

---
+++ 3-TronyaginaAlexandra-D14/3-TronyaginaAlexandra-D14/LabDHeader.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#pragma warning(disable: 4996)
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TRUE 1
+#define FALSE (!TRUE)
+
+	typedef struct {
+		int** cells;
+		int n;
+		int solved;
+	}sudoku_t;
+
+	sudoku_t* readFile(const char* fileName);
+	void printFile(const char* fileName, sudoku_t* sudoku);
+	int cellCheck(sudoku_t* sudoku, int column, int row, int num);
+	void sudokuSolution(sudoku_t* sudoku, int num, int row, int column);
+	void freeSudoku(sudoku_t* sudoku);
+
+#ifdef __cplusplus
+}
+#endif
