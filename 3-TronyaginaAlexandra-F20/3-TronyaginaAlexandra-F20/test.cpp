6def0efd4b54475aa2fc680fc1936e429add8f51
tronyaginaa <tronyaginaa@gmail.com>
2021-12-26 17:29:40 +0300

    Update test.cpp

--- 3-TronyaginaAlexandra-F20/3-TronyaginaAlexandra-F20/test.cpp
+++ 3-TronyaginaAlexandra-F20/3-TronyaginaAlexandra-F20/test.cpp
@@ -10,60 +10,58 @@ TEST(FunctionalTest, kMin_Even) {
 	sol(input, output, 6);
 	fclose(input);
 	fclose(output);
-	int key;
+	int keyExpected, keyOutput;
 	int i = 1;
-	int expected[6] = { 2, 3, 4, 5, 7, 8 };
-	int* result = (int*)malloc(sizeof(int));
+	FILE* expected = fopen("expected_even.txt", "r");
+	ASSERT_TRUE(expected);
 	output = fopen("output_even.txt", "r");
 	ASSERT_TRUE(output);
 	fseek(output, 0, SEEK_END);
-	i = ftell(output);
-	EXPECT_TRUE(i > 0);
-	fscanf(output, "%i", key);
-	result[0] = key;
-	i = 1;
-	while (fscanf(output, "%i", key) != EOF) {
-		result =(int*) realloc(result, sizeof(int));
-		result[i] = key;
-		i++;
-	}
-	EXPECT_EQ(i, 6);
-	for ( i = 0; i < 6; i++)
-		EXPECT_EQ(result[i], expected[i]);
+	fseek(expected, 0, SEEK_END);
+	EXPECT_EQ(ftell(output), ftell(expected));
+	fseek(output, 0, SEEK_SET);
+	fseek(expected, 0, SEEK_SET);
+	while (fscanf(output, "%i", &keyOutput) != EOF && fscanf(expected, "%i", &keyExpected) != EOF)
+		EXPECT_EQ(keyExpected, keyOutput);
+	fclose(output);
+	fclose(expected);
 }
 
 TEST(FunctionalTest, kMin_Odd) {
 	FILE* input = fopen("input.txt", "r");
 	ASSERT_TRUE(input);
-	FILE* output = fopen("output_even.txt", "w");
+	FILE* output = fopen("output_odd.txt", "w");
 	ASSERT_TRUE(output);
-	sol(input, output, 6);
+	sol(input, output, 7);
 	fclose(input);
 	fclose(output);
 	int key;
 	int i = 0;
-	output = fopen("output_even.txt", "r");
+	output = fopen("output_odd.txt", "r");
 	ASSERT_TRUE(output);
 	fseek(output, 0, SEEK_END);
 	i = ftell(output);
 	EXPECT_EQ(i, 1);
-	fscanf(output, "%i", key);
+	fseek(output, 0, SEEK_SET);
+	fscanf(output, "%i", &key);
 	EXPECT_EQ(9, key);
+	fclose(output);
 }
 
 TEST(FunctionalTest, notFound) {
 	FILE* input = fopen("input.txt", "r");
 	ASSERT_TRUE(input);
-	FILE* output = fopen("output_even.txt", "w");
+	FILE* output = fopen("output_notFound.txt", "w");
 	ASSERT_TRUE(output);
-	sol(input, output, 6);
+	sol(input, output, 10);
 	fclose(input);
 	fclose(output);
 	int key;
 	int i = 0;
-	output = fopen("output_even.txt", "r");
+	output = fopen("output_notFound.txt", "r");
 	ASSERT_TRUE(output);
 	fseek(output, 0, SEEK_END);
 	i = ftell(output);
 	EXPECT_EQ(i, 0);
+	fclose(output);
 }
